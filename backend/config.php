fc06d0b8141a486aa9f8a56f4a780309c2d6c0c3
LIMONCHI Rene <rene.limonchi@forvia.com>
2026-05-27 11:45:39 -0600

    fix index

--- backend/config.php
+++ backend/config.php
@@ -6,7 +6,7 @@ $script = $_SERVER['SCRIPT_NAME'];
 if (strpos($script, '/addendas/') === 0) {
     $base = '/addendas';
 } else {
-    $base = '';
+    $base = 'www.addendasfacil.com';
 }
 
 define('BASE_URL', $base);
