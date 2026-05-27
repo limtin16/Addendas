cc3223c9db8bde6560e0ff79c79310a644283ea7
LIMONCHI Rene <rene.limonchi@forvia.com>
2026-05-27 13:03:42 -0600

    fix db parameters

--- backend/db.php
+++ backend/db.php
@@ -1,9 +1,17 @@
 <?php
 
-$host = 'localhost';
-$db   = 'addendas';   // 👈 ESTA ES LA BASE QUE CREASTE
-$user = 'root';
-$pass = '';
+// ✅ detectar si estás en localhost
+if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
+    $host = 'localhost';
+    $db   = 'addendas';   // 👈 ESTA ES LA BASE QUE CREASTE
+    $user = 'root';
+    $pass = '';
+} else {
+    $host = 'localhost';
+    $db   = 'desenti2_addendas';   // 👈 ESTA ES LA BASE QUE CREASTE
+    $user = 'desenti2_af_admin';
+    $pass = 'Dani1687';
+}
 
 $conn = new mysqli($host, $user, $pass, $db);
 
