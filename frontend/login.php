78030bf08ac356a9c4cb7936dbc5369d920e733d
LIMONCHI Rene <rene.limonchi@forvia.com>
2026-05-27 12:40:35 -0600

    fix redirects

--- frontend/login.php
+++ frontend/login.php
@@ -1,11 +1,10 @@
 <?php
-
 $path="";
 echo getcwd();
-exit;
+
 $count= (substr_count(substr(getcwd(),strrpos(getcwd(),'addenda'),100),'\\'));
-if $count==0){
-    $count= (substr_count(substr(getcwd(),strrpos(getcwd(),'addendafacil.com'),100),'/'));
+if ($count==0){
+    $count= (substr_count(substr(getcwd(),strrpos(getcwd(),'addenda'),100),'\\'));
 }
 for ($i=0; $i<$count; $i++){
 	$path.="../";
