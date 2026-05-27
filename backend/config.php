f6c0888bad634c02868a592d715142d1a188a23d
LIMONCHI Rene <rene.limonchi@forvia.com>
2026-05-27 11:39:01 -0600

    fix index

--- backend/config.php
+++ backend/config.php
@@ -2,13 +2,13 @@
 
 // ✅ detectar base automáticamente
 $script = $_SERVER['SCRIPT_NAME'];
-echo "script". $script;;
+
 if (strpos($script, '/addendas/') === 0) {
     $base = '/addendas';
 } else {
-    $base = '';
+    $base = 'www.addendasfacil.com';
 }
-echo "BASE: " . $base;;
+
 define('BASE_URL', $base);
 define('BACKEND_ROOT', __DIR__);
 define('TEMPLATE_STORAGE_PATH', BACKEND_ROOT . '/Storage/templates');
