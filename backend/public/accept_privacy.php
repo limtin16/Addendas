c3d64f0d73e34a7e307b5df8aa09dba3cbd7aa87
LIMONCHI Rene <limtin16@gmail.com>
2026-06-01 11:17:06 -0600

    habilitando los mails

--- backend/public/accept_privacy.php
+++ backend/public/accept_privacy.php
@@ -1,8 +1,17 @@
 <?php
+$path="";
+$count= (substr_count(substr(getcwd(),strrpos(getcwd(),'addenda'),100),'\\'));
+if ($count==0){
+    $count= (substr_count(substr(getcwd(),strrpos(getcwd(),'addendafacil.com'),100),'/'));
+}
+for ($i=0; $i<$count; $i++){
+	$path.="../";
+}
+$path.="backend/config.php";
 require_once $path;
 
 session_start();
-require_once dirname(__DIR__) . '/../db.php';
+require_once dirname(__DIR__) . '../db.php';
 
 $userId = $_SESSION['user_id'] ?? null;
 
@@ -11,14 +20,15 @@ if (!$userId) {
     exit;
 }
 
+// obtener datos técnicos
 $ip = $_SERVER['REMOTE_ADDR'];
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 
+// obtener versión activa
 $stmt = $conn->prepare("
     SELECT version FROM privacy_policy WHERE active = 1 LIMIT 1
 ");
 $stmt->execute();
-
 $policy = $stmt->get_result()->fetch_assoc();
 $version = $policy['version'] ?? '1.0';
 
@@ -31,5 +41,8 @@ $stmt = $conn->prepare("
 $stmt->bind_param("isss", $userId, $ip, $userAgent, $version);
 $stmt->execute();
 
+
+
+// redirigir al sistema
 header("Location: " . BASE_URL . "/frontend/dashboard.php");
 exit;
