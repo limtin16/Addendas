9c18368788fac380a622f6629f8caccad6fb8edc
LIMONCHI Rene <limtin16@gmail.com>
2026-06-26 15:00:30 -0600

    multicfdi enabled test

--- backend/public/save_generated_cfdi.php
+++ backend/public/save_generated_cfdi.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-ini_set('display_errors', 0);
-error_reporting(0);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 session_start();
 header('Content-Type: application/json');
